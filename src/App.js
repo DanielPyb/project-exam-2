0371a13c632dc56d66ab589bc98f1c8d796f9791
Daniel Sollid <dpsollid@hotmail.no>
2023-05-01 18:09:42 +0200

    first version of the forms are completed.  Login, register and new venue

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import NewVenueForm from './components/NewVenueForm';
+import RegisterLoginForm from './components/RegisterLoginForm';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RegisterLoginForm />
     </div>
   );
 }
