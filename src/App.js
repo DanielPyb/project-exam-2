a331c7a7e6a5938e032196de46ac68c5d3669fec
Daniel Sollid <dpsollid@hotmail.no>
2023-05-04 23:10:15 +0200

    set up landing site

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import NewVenueForm from './components/NewVenueForm';
 import RegisterLoginForm from './components/RegisterLoginForm';
+import LandingSite from './pages/LandingSite';
 
 
 function App() {
   return (
     <div>
-      <RegisterLoginForm />
+      <LandingSite />
     </div>
   );
 }
---
+++ src/pages/LandingSite.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+
+export default function LandingSite() {
+  return (
+    <div className='landing-page'>
+    <div className='hero'>
+        <h1 className='hero-title'>HoliDaze</h1>
+        <h4 className='hero-text'>Experience vacations how they were meant to be!</h4>
+        <Button>sign up</Button>
+        <p className='hero-link'><a href="">tour site</a></p>
+    </div>
+    </div>
+  )
+}
